384aaaeee6389ad27c25cfa2927349d25cb3e896
Mridhul2k03 <mridhulkrishna11@gmail.com>
2025-07-30 15:25:52 +0530

    updated

--- Application/models.py
+++ Application/models.py
@@ -119,6 +119,7 @@ class HomePageSliderImage(models.Model):
     def __str__(self):
         return self.title
 
+
 class AboutUsImages(models.Model):
     title = models.CharField(max_length=255)
     image = models.ImageField(upload_to='about_us_images/')
--- Project/settings.py
+++ Project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'Application',
     'corsheaders',
+    'Dashboard',
 ]
 
 MIDDLEWARE = [
@@ -183,6 +184,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = '' 
+EMAIL_HOST_USER = 'supermarine.jetski@gmail.com'
+EMAIL_HOST_PASSWORD = 'mndv eopw opuj diek' 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
