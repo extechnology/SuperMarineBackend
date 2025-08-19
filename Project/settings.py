71902b08a77f7126f8cdcbdb76f07368a801b35a
VIPINRAJK26 <vipinrajk026@gmail.com>
2025-08-19 15:41:15 +0530

    updated requirements.txt

--- Project/settings.py
+++ Project/settings.py
@@ -158,7 +158,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),  # Customize as needed
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),  
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
