eea83f09d67367a59415b74405c89e696b3a947d
Mridhul2k03 <mridhulkrishna11@gmail.com>
2025-08-19 10:36:13 +0530

    model update

--- Application/models.py
+++ Application/models.py
@@ -60,7 +60,14 @@ class Vehicle(models.Model):
     def __str__(self):
         return str(self.name - self.category.name)
 
-
+class VehicleDuration(models.Model):
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, related_name='durations')
+    duration = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return f"{self.vehicle.name} - {self.duration} - {self.price}"
+    
 class Booking(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
